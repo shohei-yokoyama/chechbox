75b5c626bfd3c81557a0ae22387f3b0693faa047
YKS <sp658qy9@gmail.com>
2021-03-03 22:21:44 +0900

    firebaseup

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import firebase from 'firebase'
 import 'firebase/firestore'
+import 'firebase/auth'
 import config from '~/firebase.config'
 
 if (!firebase.apps.length) {
