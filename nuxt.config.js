9f853f1e853f90d72146e622702487e079ed45b8
YKS <sp658qy9@gmail.com>
2021-03-04 23:01:26 +0900

    google

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,10 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        name: 'google-site-verification',
+        content: 'h22HgZA6BANiZVw2u2uXWs1DZtVPCjPoj0AMDgB28n0',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
